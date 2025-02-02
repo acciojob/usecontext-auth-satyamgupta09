9bbc765f0ec73e081f635d65e5ffa17cbfbac7ed
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-02-03 02:09:12 +0530

    Update AuthContext.js

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState } from "react";
 import Status from "./Status";
 export const AuthenticationContext = createContext();
 
