b9e18aa4ed88bd4acf7c01d4769f479ed95e5a68
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-02-03 01:58:38 +0530

    Create AuthContext.js

---
+++ src/components/AuthContext.js
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+import Status from "./Status";
+export const AuthenticationContext = createContext();
+
+export default function AuthContext() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  handleChange = (e) => {
+    console.log(e.target.checked);
+    setIsAuthenticated(e.target.checked);
+  };
+  return (
+    <div>
+      <AuthenticationContext.Provider value={isAuthenticated}>
+        <Status />
+      </AuthenticationContext.Provider>
+      <input type="checkbox" onChange={handleChange} />
+      <label>I'm not a robot</label>
+    </div>
+  );
+}
